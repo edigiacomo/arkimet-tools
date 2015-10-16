1307db4fdd35e88da82af5db39b07457c5c9b0a4
Emanuele Di Giacomo <emanuele.digiacomo@gmail.com>
2015-10-16 17:12:05 +0200

    arkitools which-datasets

--- arkitools.py
+++ arkitools.py
@@ -82,6 +82,24 @@ def repack_archived_file(infile, backup_file=None, dry_run=False):
             shutil.copyfile(outfile, infile)
 
 
+def which_datasets(infile, dsconf):
+    """Given a mergeconf, return the dataset paths that would acquire the
+    file."""
+    from configparser import ConfigParser
+    from subprocess import check_output, DEVNULL
+    cfg = ConfigParser()
+    cfg.read([dsconf])
+    for s in cfg.sections():
+        if not "filter" in cfg[s] or not "path" in cfg[s]:
+            continue
+        p = cfg.get(s, "path")
+        f = cfg.get(s, "filter")
+        r = check_output(["arki-query", "--summary", "--dump", f, infile],
+                         stderr=DEVNULL)
+        if r and not r.isspace():
+            yield p
+
+
 def do_clone_dataset(args):
     return clone_dataset(src_ds=args.srcds, dst_ds=args.dstds)
 
@@ -92,6 +110,11 @@ def do_repack_archived_file(args):
         backup_file=args.backup_file,
         dry_run=args.dry_run
     )
+
+
+def do_which_datasets(args):
+    for ds in which_datasets(infile=args.infile, dsconf=args.conf):
+        print(ds)
 
 
 if __name__ == '__main__':
@@ -115,6 +138,12 @@ if __name__ == '__main__':
     repack_archived_file_p.add_argument("-b", "--backup-file", help="Save original data")
     repack_archived_file_p.add_argument("infile", help="File to repack")
     repack_archived_file_p.set_defaults(func=do_repack_archived_file)
+
+    # Which dataset
+    which_dataset_p = subparsers.add_parser('which-datasets', description="List dataset paths that would acquire the file")
+    which_dataset_p.add_argument('conf', help="Config file about input sources")
+    which_dataset_p.add_argument('infile', help="File to inspect")
+    which_dataset_p.set_defaults(func=do_which_datasets)
 
     args = parser.parse_args()
     args.func(args)
