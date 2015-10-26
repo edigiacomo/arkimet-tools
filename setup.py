27e3ca84ba56eeefc5c7714f3cb95826ecbf57c6
Emanuele Di Giacomo <emanuele.digiacomo@gmail.com>
2015-10-26 15:58:58 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="arkimet-tools",
+    version="0.1",
+    description="Arkimet tools",
+    url="https://github.com/edigiacomo/arkimet-tools",
+    author="Emanuele Di Giacomo",
+    author_email="edigiacomo@arpa.emr.it",
+    license="GPLv2+",
+    packages=["arkitools"],
+    entry_points={
+        'console_scripts': ['arkitools-cli=arkitools.cli:main'],
+    },
+)
