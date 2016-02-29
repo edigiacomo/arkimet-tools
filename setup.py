3af9eedfd93996da75f623e369a466f350eec6eb
Emanuele Di Giacomo <emanuele.digiacomo@gmail.com>
2016-02-29 14:35:59 +0100

    v0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="arkimet-tools",
-    version="0.1",
+    version="0.3",
     description="Arkimet tools",
     url="https://github.com/edigiacomo/arkimet-tools",
     author="Emanuele Di Giacomo",
